39c71849b076f3cbe120e7137f9cb377f1b5b2c5
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2025-06-09 12:54:36 +0500

    topgrade

--- home.nix
+++ home.nix
@@ -1,7 +1,18 @@
-_:
+{ lib, ... }:
 {
   home-manager = {
     users.shakhzod = {
+      programs = {
+        topgrade = {
+          enable = true;
+          settings = {
+            linux = lib.mkForce {
+              nix_arguments = "--flake github:shakhzodkudratov/hetzner-nixos --option tarball-ttl 0";
+            };
+          };
+        };
+      };
+
       home = {
         stateVersion = "25.05";
       };
